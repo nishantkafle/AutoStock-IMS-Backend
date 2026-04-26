2484f88c2fe910dd14155345a5cbe0053504e694
Pritha Thapa <prithathapa09@gmail.com>
2026-04-26 19:25:19 +0545

    Add CustomerDtos for customer registration, response and report

--- AutoStock.API/Program.cs
+++ AutoStock.API/Program.cs
@@ -84,7 +84,9 @@ builder.Services.AddRateLimiter(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact", policy =>
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173",
+        "http://localhost:5174"
+        )
               .AllowAnyHeader()
               .AllowAnyMethod());
 });
