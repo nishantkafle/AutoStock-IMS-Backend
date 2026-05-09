0d32d42f7472f0261d64f840d89ad4e169bfd4f6
Pritha Thapa <prithathapa09@gmail.com>
2026-05-09 18:30:24 +0545

    Migration : Add TotalSpent field to User entity

---
+++ AutoStock.Infrastructure/Migrations/20260509124306_AddTotalSpentToUser.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoStock.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalSpentToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasPendingCredit",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalSpent",
+                table: "AspNetUsers",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasPendingCredit",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TotalSpent",
+                table: "AspNetUsers");
+        }
+    }
+}
